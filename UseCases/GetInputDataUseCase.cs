c01187f430bc34efb6a6559c9486ff56c4c32239
Felix Watine <felix.watine@masteos.com>
2024-01-29 12:56:38 +0100

    add with recursion

--- UseCases/GetInputDataUseCase.cs
+++ UseCases/GetInputDataUseCase.cs
@@ -14,29 +14,25 @@ public class GetInputDataUseCase
     public Dictionary<string, string> GetData()
     {
         var json = File.ReadAllText(this.inputPath);
-
         using JsonDocument doc = JsonDocument.Parse(json);
         var dic = new Dictionary<string, string>();
-        foreach (var property in doc.RootElement.EnumerateObject())
+        ParseJsonElement(doc.RootElement, dic, null);
+        return dic;
+    }
+
+    private void ParseJsonElement(JsonElement element, Dictionary<string, string> dic, string? prefix)
+    {
+        foreach (var property in element.EnumerateObject())
         {
-            var element = property.Value;
-            if (element.ValueKind == JsonValueKind.String)
+            var currentKey = prefix != null ? $"{prefix}.{property.Name}" : property.Name;
+            if (property.Value.ValueKind == JsonValueKind.String)
             {
-                dic[property.Name] = element.GetString()!;
+                dic[currentKey] = property.Value.GetString()!;
             }
-            else
+            else if (property.Value.ValueKind == JsonValueKind.Object)
             {
-                foreach (var subProperty in element.EnumerateObject())
-                {
-                    var subElement = subProperty.Value;
-                    if (subElement.ValueKind == JsonValueKind.String)
-                    {
-                        dic[$"{property.Name}.{subProperty.Name}"] = subElement.GetString()!;
-                    }
-                }
+                ParseJsonElement(property.Value, dic, currentKey);
             }
         }
-
-        return dic;
     }
 }
