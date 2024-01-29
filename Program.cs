fe90de09b32efa4336e63124e255ae6e131176ee
Felix Watine <felix.watine@masteos.com>
2024-01-29 14:50:12 +0100

    works fine

--- Program.cs
+++ Program.cs
@@ -1,9 +1,6 @@
-﻿using HTMLQuestPDF.Extensions;
-using pompadoc.UseCases;
+﻿using pompadoc.UseCases;
 using QuestPDF.Fluent;
-using QuestPDF.Helpers;
 using QuestPDF.Infrastructure;
-using QuestPDF.Previewer;
 
 Console.WriteLine("####Bienvenue sur Pompadoc####");
 
@@ -11,43 +8,13 @@ Console.WriteLine("####Bienvenue sur Pompadoc####");
  //string inputPath = inputPathUseCase.GetPath();
 string inputPeoplePath = @"D:\Dev\MyProjects\pompadoc\Data\input-people-jason-burne.json";
 string inputTemplatePath = @"D:\Dev\MyProjects\pompadoc\Data\input-template-resiliation-banque.html";
-//
+
 Dictionary<string,string> input = new GetInputDataUseCase(inputPeoplePath).GetData();
-string html = File.ReadAllText(inputTemplatePath);
+string htmlTemplate = File.ReadAllText(inputTemplatePath);
 
 QuestPDF.Settings.License = LicenseType.Community;
-var document  = Document.Create(container =>
-{
-    container.Page(page =>
-    {
-        page.Size(PageSizes.A4);
-        page.Margin(2, Unit.Centimetre);
-        page.Background(Colors.White);
-        page.DefaultTextStyle(x => x.FontSize(20));
-
-        page.Header()
-            .Text($"Données concernant {input["etatCivil.nom"]}")
-            .SemiBold().FontSize(36).FontColor(Colors.Blue.Medium);
-
-        page.Content()
-            .PaddingVertical(1, Unit.Centimetre)
-            .Column(x =>
-            {
-                x.Spacing(20);
-
-                x.Item().Text(Placeholders.LoremIpsum());
-                x.Item().Image(Placeholders.Image(200, 100));
-                x.Item().HTML(descriptor => descriptor.SetHtml(html));
-            });
-
-        page.Footer()
-            .AlignCenter()
-            .Text(x =>
-            {
-                x.Span("Page ");
-                x.CurrentPageNumber();
-            });
-    });
-});
+var document  = new CreateDocumentUseCase(input).Create(htmlTemplate);
 //document.ShowInPreviewer();
-document.GeneratePdf("hello.pdf");
+
+document.GeneratePdf("resiliation-banque.pdf");
+
