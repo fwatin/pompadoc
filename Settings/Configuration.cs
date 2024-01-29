7f1c6349f5930dcffcb00fb03e4f8a10f024a3ae
Felix Watine <felix.watine@masteos.com>
2024-01-29 15:43:47 +0100

    add error message

--- Settings/Configuration.cs
+++ Settings/Configuration.cs
@@ -11,13 +11,22 @@ public class Configuration
         string configurationPath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
         Configuration? config = JsonSerializer.Deserialize<Configuration>(File.ReadAllText(configurationPath));
         
-        if (config is null) throw new("Configuration is null after deserialisation");
+        if (config is null) throw new("Configuration is null after deserialization");
         if (config.Path is null) throw new("Expected Path attribute in appsettings but Path is null");
-        if (config.Path!.InputPeoplePath is null) throw new("Expected InputPeoplePath attribute in appsettings.Path but InputPeoplePath is null");
-        if (config.Path!.InputTemplatePath is null) throw new("Expected InputTemplatePath attribute in appsettings.Path but InputTemplatePath is null");
-        if (config.Path!.OutputResultPath is null) throw new("Expected OutputResultPath attribute in appsettings.Path but OutputResultPath is null");
+        
+        VerifyPath("InputPeoplePath",config.Path!.InputPeoplePath,configurationPath);
+        VerifyPath("InputTemplatePath",config.Path!.InputTemplatePath,configurationPath);
+        VerifyPath("OutputResultPath",config.Path!.OutputResultPath,configurationPath);
         
         return config!;
+    }
+
+    private static void VerifyPath(string name, string? path, string configurationPath)
+    {
+        if (path is null) throw new($"Expected {name} attribute in appsettings.Path but {name} is null");
+        if (File.Exists(path) is false)
+            throw new(
+                $"File supposedly in {path} doesnt exist. Verify configuration file (appsettings.json) to update your paths in {configurationPath}");
     }
 }
 
