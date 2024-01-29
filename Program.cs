bdea4bbb027329fbf9868815c13cf014c15a0116
Felix Watine <felix.watine@masteos.com>
2024-01-29 15:20:20 +0100

    works fine

--- Program.cs
+++ Program.cs
@@ -8,19 +8,24 @@ Console.WriteLine("####Bienvenue sur Pompadoc####");
 
 string configurationPath = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
 Configuration configuration = JsonSerializer.Deserialize<Configuration>(File.ReadAllText(configurationPath))!;
-  
-// GetInputPathUseCase inputPathUseCase = new();
- //string inputPath = inputPathUseCase.GetPath();
-string inputPeoplePath = @"D:\Dev\MyProjects\pompadoc\Data\people\input-people-jason-burne.json";
-string inputTemplatePath = @"D:\Dev\MyProjects\pompadoc\Data\templates\input-template-resiliation-banque.html";
-
-Dictionary<string,string> input = new GetInputDataUseCase(inputPeoplePath).GetData();
-string htmlTemplate = File.ReadAllText(inputTemplatePath);
-
 QuestPDF.Settings.License = LicenseType.Community;
-var document  = new CreateDocumentUseCase(input).Create(htmlTemplate);
-//document.ShowInPreviewer();
 
-document.GeneratePdf("resiliation-banque.pdf");
-Console.WriteLine("new file generated: resiliation-banque.pdf");
+IEnumerable<string> allPeople = Directory.EnumerateFiles(configuration.Path!.InputPeoplePath!);
+string[] allTemplates = Directory.EnumerateFiles(configuration.Path!.InputTemplatePath!).ToArray();
+if (Directory.Exists(configuration.Path.OutputResultPath) is false)
+    Directory.CreateDirectory(configuration.Path.OutputResultPath!);
 
+foreach (string personFilePath in allPeople)
+{
+    Dictionary<string, string> input = new GetInputDataUseCase(personFilePath).GetData();
+
+    foreach (string templatePath in allTemplates)
+    {
+        string htmlTemplate = File.ReadAllText(templatePath);
+        string templateName = Path.GetFileNameWithoutExtension(templatePath);
+        Document document = new CreateDocumentUseCase(input).Create(htmlTemplate);
+        string outputPath = Path.Combine(configuration.Path.OutputResultPath!, $"{templateName}.pdf");
+        document.GeneratePdf(outputPath);
+        Console.WriteLine($"new file generated: {templateName}.pdf");
+    }
+}
