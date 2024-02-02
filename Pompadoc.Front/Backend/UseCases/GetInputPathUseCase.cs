00d7bc918de2ee38919f26a730de2677d3b18a84
Felix Watine <felix.watine@masteos.com>
2024-02-02 17:57:55 +0100

    import old code

---
+++ Pompadoc.Front/Backend/UseCases/GetInputPathUseCase.cs
@@ -0,0 +1,12 @@
+﻿namespace pompadoc.UseCases;
+
+public class GetInputPathUseCase
+{
+    public string GetPath()
+    {
+        Console.WriteLine("Entrer le chemin ou se trouve les jsons contenant les détails des défunts:");
+        string? path = Console.ReadLine();
+        if(path is null) throw new($"Path is null");
+        return path!;
+    }
+}
